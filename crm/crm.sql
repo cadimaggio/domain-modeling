6d91a31392914e1a669e387185be9f1fb927bd77
Christina DiMaggio <christina.dimaggio@kellogg.northwestern.edu>
2023-04-27 16:37:46 +0000

    finished

--- crm/crm.sql
+++ crm/crm.sql
@@ -1,5 +1,20 @@
 -- Drops existing tables, so we start fresh with each
 -- run of this script
 -- e.g. DROP TABLE IF EXISTS ______;
+DROP TABLE 
 
 -- CREATE TABLES
+CREATE TABLE contacts (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    first_name TEXT,
+    last_name TEXT,
+    email TEXT,
+    phone_number TEXT
+);
+
+CREATE TABLE activities (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    call TEXT,
+    email TEXT,
+    date TEXT,
+)
